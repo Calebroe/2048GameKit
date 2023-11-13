bae7225906079902e831ce0132ee318427247863
Caleb <100630232+Calebroe@users.noreply.github.com>
2023-11-13 18:04:33 -0500

    adding framework files
    game logic and essential framework

---
+++ IOS2048game/IOS2048game/IOS2048gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS2048gameApp.swift
+//  IOS2048game
+//
+//  Created by Caleb on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct IOS2048gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IOS2048game/IOS2048game/MainScreen.swift
@@ -0,0 +1,20 @@
+//
+//  MainScreen.swift
+//  IOS2048game
+//
+//  Created by Caleb on 11/13/23.
+//
+
+import SwiftUI
+
+struct MainScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        MainScreen()
+    }
+}
