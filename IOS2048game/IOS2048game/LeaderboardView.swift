b230409ab2d194c3f77b25ce8b0a5b6d9e75d5f5
peyerkco <peyerkco@mail.gvsu.edu>
2023-12-05 13:09:32 -0500

    Changed the bundle identifier and added the Developer account/team.

--- IOS2048game/IOS2048game/LeaderboardView.swift
+++ IOS2048game/IOS2048game/LeaderboardView.swift
@@ -9,14 +9,6 @@ import SwiftUI
 import GameKit
 
 
-//struct LeaderboardView: UIViewControllerRepresentable {
-//    func makeUIViewController(context: Context) -> some UIViewController {
-//        GKGameCenterViewController(state: .leaderboards)
-//    }
-//
-//    func updateUIViewController( _ uiViewController: UIViewControllerType, context: Context) {}
-//}
-
 struct LeaderboardView: UIViewControllerRepresentable {
     var leaderboardIdentifier: String
 
@@ -29,9 +21,8 @@ struct LeaderboardView: UIViewControllerRepresentable {
     func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
 
     private func showLeaderboard(viewController: UIViewController) {
-        let leaderboardViewController = GKGameCenterViewController()
+        let leaderboardViewController = GKGameCenterViewController(state: .leaderboards)
         leaderboardViewController.gameCenterDelegate = viewController as? GKGameCenterControllerDelegate
-        leaderboardViewController.viewState = .leaderboards
         leaderboardViewController.leaderboardIdentifier = leaderboardIdentifier
 
         viewController.present(leaderboardViewController, animated: true, completion: nil)
@@ -40,6 +31,7 @@ struct LeaderboardView: UIViewControllerRepresentable {
 
 struct LeaderboardView_Previews: PreviewProvider {
     static var previews: some View {
+        // TODO: Update this field with proper ID
         LeaderboardView(leaderboardIdentifier: "HELP")
     }
 }
