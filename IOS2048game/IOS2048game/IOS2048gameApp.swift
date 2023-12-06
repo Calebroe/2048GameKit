f6daf1e30ab405658501989da0f1a4aca5dc1848
peyerkco <peyerkco@mail.gvsu.edu>
2023-12-06 14:07:50 -0500

    Added extra GameCenter auth check on app launch. Added trophy.png for achievements

--- IOS2048game/IOS2048game/IOS2048gameApp.swift
+++ IOS2048game/IOS2048game/IOS2048gameApp.swift
@@ -9,9 +9,21 @@ import SwiftUI
 
 @main
 struct IOS2048gameApp: App {
+    @StateObject private var gameCenterHandler = GameCenterHandler()
+    
     var body: some Scene {
         WindowGroup {
             MainScreen()
+                .onAppear() {
+                    if gameCenterHandler.isPresentingAuthView {
+                        GameCenterAuthenticator(presentationController: UIApplication.shared.windows.first?.rootViewController ?? UIViewController())
+                                .edgesIgnoringSafeArea(.all)
+                                .onDisappear() {
+                                    // Reset the view variable so it won't re-appear
+                                    gameCenterHandler.isPresentingAuthView = false
+                                }
+                    }
+                }
         }
     }
 }
